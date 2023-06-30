ba450f0c827e06e36d6a464940abdaf3c9783109
Milan Bandara <e18041@eng.pdn.ac.lk>
2023-06-30 06:38:07 +0530

    Better augmentation code.

--- augment_data.py
+++ augment_data.py
@@ -2,38 +2,44 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-
 def time_shift_left(shift):
     fall_folder_path = "D:/Product_design/Fall-detection-device/training_data"
     fall_files = os.listdir(fall_folder_path)
     length = len(fall_files)
+    Augmented_data_path = "D:/Product_design/Fall-detection-device/Augmented_data"
+    Aug_files = os.listdir(Augmented_data_path)
+    Aug_length = len(Aug_files)
 
     for i in range(1,len(fall_files)+1):
-        #skip the data with wrong shape
         shifted_time = shift
         data = np.load('D:/Product_design/Fall-detection-device/training_data/data'+str(i)+".npy")
         selected = data[1:,shifted_time:]
         selected2 = data[1:,(1000-shifted_time):]
         data[1:,0:(1000-shifted_time)] = selected
         data[1:,(1000-shifted_time):] = selected2
-        np.save('D:/Product_design/Fall-detection-device/Augmented_data/data'+str(i)+'.npy', data)
+        np.save('D:/Product_design/Fall-detection-device/Augmented_data/data'+str(i+Aug_length)+'.npy', data)
 def time_shift_right(shift):
     #complete this
     fall_folder_path = "D:/Product_design/Fall-detection-device/training_data"
     fall_files = os.listdir(fall_folder_path)
-    length = len(fall_files)
+    Augmented_data_path = "D:/Product_design/Fall-detection-device/Augmented_data"
+    Aug_files = os.listdir(Augmented_data_path)
+    Aug_length = len(Aug_files)
 
     for i in range(1,len(fall_files)+1):
         #skip the data with wrong shape
         shifted_time = shift
         data = np.load('D:/Product_design/Fall-detection-device/training_data/data'+str(i)+".npy")
-        selected = data[1:,shifted_time:]
-        selected2 = data[1:,(1000-shifted_time):]
-        data[1:,0:(1000-shifted_time)] = selected
-        data[1:,(1000-shifted_time):] = selected2
-        np.save('D:/Product_design/Fall-detection-device/Augmented_data/data'+str(i)+'.npy', data)
+        selected = data[1:,0:(1000-shifted_time)]
+        selected2 = data[1:,0:shifted_time]
+        data[1:,shifted_time:] = selected
+        data[1:,0:shifted_time] = selected2
+        np.save('D:/Product_design/Fall-detection-device/Augmented_data/data'+str(i+Aug_length)+'.npy', data)
         
 time_shift_left(200)
+time_shift_right(200)
+time_shift_left(100)
+time_shift_right(100)
 
 data1 = np.load('D:/Product_design/Fall-detection-device/training_data/data4.npy')[1:,:]
 data2 = np.load('D:/Product_design/Fall-detection-device/Augmented_data/data4.npy')[1:,:]
